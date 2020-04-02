3bec3ce14567db0c36f106ef6406b6d8d872aa8a
bramospio <59071943+bramospio@users.noreply.github.com>
2020-04-02 01:03:39 -0300

    Add files via upload

--- R/survChildren.R
+++ R/survChildren.R
@@ -1,8 +1,7 @@
 #'surv.children
 #'
 #' Put description
-#'@import tidyverse
-#'@import rapportools
+#'
 #'@param data  The data.frame must have seven variables in the following order: 'age groups (AG)', 'number of women by AG', 'location', 'number of live births from women in each AG', number of surviving childern from women in each AG', 'children sex', 'year'.
 #'
 #'
